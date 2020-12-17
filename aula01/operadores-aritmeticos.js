cce7acad9382c0600364860d9d9b1301e78e4641
Pedro Ivo <ivo.mqrs@gmail.com>
2020-12-16 21:32:30 -0300

    Aula 01 - operadores aritmeticos

---
+++ aula01/operadores-aritmeticos.js
@@ -0,0 +1,25 @@
+// operadores + , - , / , * 
+
+//soma 
+var sum = 1 + 3 ;
+sum++;
+sum += 1;
+console.log('Soma' , sum);
+
+//subtracao
+var sub = 3 - 1; 
+sub--;
+sub -= 1;
+console.log('Subtracao' , sub);
+
+//multiplicacao
+var mult = 3 * 3;
+mult *= 2;
+console.log('Multiplicacao' , mult);
+
+//divisao
+var div = 10/2;
+div /= 2;
+console.log('Divisao' , div);
+
+
---
+++ aula01/variaveis-tipos-dados.js
@@ -0,0 +1,30 @@
+//number
+var z = 1;
+console.log('Number ->', z);
+console.log('Type Of Number ->', typeof z ==='number');
+
+//string
+var x = 'hello js';
+console.log('String -> ', x);
+console.log('Type Of String ->', typeof x  ==='string');
+
+//boolean 
+var y = true;
+console.log('Boolean ->', y);
+console.log('Type Of Boolean ->', typeof y  ==='boolean');
+
+//null 
+var v = null;
+console.log('Null ->', v);
+console.log('Type Of Null (Object) ->',  typeof v === 'object');
+console.log('Type Of Null (Null) ->',  typeof v === 'null');
+
+//null 
+var t = undefined;
+console.log('Undefined ->', t);
+console.log('Type Of undefined ->', typeof t  ==='undefined');
+
+//object
+var pessoa = {nome : 'Pedro' , idade : 32};
+console.log('Object ->', pessoa);
+console.log('Type Of Object ->', typeof pessoa  ==='object');
