371fc38d368195b5ade856d11b7eb53aefaef99f
Pedro Ivo <ivo.mqrs@gmail.com>
2020-12-23 21:01:44 -0300

    Aula 02 - Instruções condicionais - IF

---
+++ aula02/instrucoes-condicionais.js
@@ -0,0 +1,22 @@
+var numero = 10;
+
+//if simples
+if(numero === 10){
+    console.log(`Valores iguais ${numero} e 10`);
+}
+
+//if else
+if(numero === 20){
+    console.log(`Valores iguais ${numero} e 10`);
+}else {
+    console.log(`Valores não são iguais ${numero} e 20`);
+}
+
+//else if
+if(numero === 30){
+    console.log(`Valores não são iguais ${numero} e 30`);
+}else if(numero === 40) {
+    console.log(`Valores iguais ${numero} e 40`);
+}else {
+    console.log('Não foi possível verificar valor!');
+}
