4c88df6532a72603cbc1ba2cc2dabf54bcacd31b
Pedro Ivo <ivo.mrqs@gmail.com>
2021-01-25 19:56:16 -0300

    Aula 02 - Instrucoes Condicionais IF

---
+++ aula02/condicionais-com-operadores-logicos.js
@@ -0,0 +1,15 @@
+var a = 10;
+var b = 11;
+
+if (a === 12 || b > 10) {
+    b = 100;
+}
+
+console.log('Valor de B atualizado : ' , b);
+
+
+if (a === 10 && b > 10) {
+    b = 200;
+}
+
+console.log('Valor de B atualizado : ' , b);
